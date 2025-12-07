e423f99dc7027b1944e574d147b9046944ecb78f
Sannteen Budda <sannteenbudda@gmail.com>
2025-12-07 14:28:05 -0500

    Login page updated and connected to database.

---
+++ Controllers/FarmProductsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AgriReachWeb.Models;
+
+namespace AgriReachWeb.Controllers
+{
+    public class FarmProductsController : Controller
+    {
+        private readonly AgriReachDbContext _context;
+
+        public FarmProductsController(AgriReachDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FarmProducts
+        public async Task<IActionResult> Index()
+        {
+            var agriReachDbContext = _context.FarmProducts.Include(f => f.Farm).Include(f => f.Product);
+            return View(await agriReachDbContext.ToListAsync());
+        }
+
+        // GET: FarmProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmProduct = await _context.FarmProducts
+                .Include(f => f.Farm)
+                .Include(f => f.Product)
+                .FirstOrDefaultAsync(m => m.FarmProductId == id);
+            if (farmProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(farmProduct);
+        }
+
+        // GET: FarmProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "FarmId");
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId");
+            return View();
+        }
+
+        // POST: FarmProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FarmProductId,FarmId,ProductId,BasePrice,AvailabilityStatus,LastUpdated,Unit")] FarmProduct farmProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(farmProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "FarmId", farmProduct.FarmId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", farmProduct.ProductId);
+            return View(farmProduct);
+        }
+
+        // GET: FarmProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmProduct = await _context.FarmProducts.FindAsync(id);
+            if (farmProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "FarmId", farmProduct.FarmId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", farmProduct.ProductId);
+            return View(farmProduct);
+        }
+
+        // POST: FarmProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FarmProductId,FarmId,ProductId,BasePrice,AvailabilityStatus,LastUpdated,Unit")] FarmProduct farmProduct)
+        {
+            if (id != farmProduct.FarmProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(farmProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FarmProductExists(farmProduct.FarmProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FarmId"] = new SelectList(_context.Farms, "FarmId", "FarmId", farmProduct.FarmId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", farmProduct.ProductId);
+            return View(farmProduct);
+        }
+
+        // GET: FarmProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var farmProduct = await _context.FarmProducts
+                .Include(f => f.Farm)
+                .Include(f => f.Product)
+                .FirstOrDefaultAsync(m => m.FarmProductId == id);
+            if (farmProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(farmProduct);
+        }
+
+        // POST: FarmProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var farmProduct = await _context.FarmProducts.FindAsync(id);
+            if (farmProduct != null)
+            {
+                _context.FarmProducts.Remove(farmProduct);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FarmProductExists(int id)
+        {
+            return _context.FarmProducts.Any(e => e.FarmProductId == id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,13 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContext<AgriReachDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<AgriReachDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -20,6 +16,13 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AgriReachDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -31,17 +34,15 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
